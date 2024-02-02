3b428bb5b29327f6b736f460b27946299677fffc
James Phillips <james.g.phillips.91@gmail.com>
2024-02-01 22:34:32 -0800

    added user post route tested

--- app/models/user_s_model.py
+++ app/models/user_s_model.py
@@ -6,5 +6,6 @@ class Users_s(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), unique=True, nullable=False)
     posts = db.relationship('Post_s', backref='author', lazy=True)
-    
+
+
 
--- app/routes/user_s_routes.py
+++ app/routes/user_s_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from app.models.user_s_model import Users_s
+from factory import db
 
 user_bp = Blueprint('user_bp', __name__)
 
@@ -10,3 +11,24 @@ def get_all_users():
     for user in users:
         user_list.append({'id': user.id, 'username': user.username, 'email': user.email})
     return jsonify({'users': user_list})
+
+@user_bp.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+
+    name = data.get('name')
+
+    if not name: 
+        return jsonify({'error': 'Missing required fields'}), 400
+    
+    existing_user = Users_s.query.filter(Users_s.name == name).first()
+    
+    if existing_user: 
+        return jsonify({'error': 'name exists'}), 400
+    
+    new_user = Users_s(name=name)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'User created successfully', 'user_id': new_user.id}), 201
